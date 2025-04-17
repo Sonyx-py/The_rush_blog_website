0a538da3d00723a2f3d3f928b88994f28f1aaf79
Sonyx-py <123290256+Sonyx-py@users.noreply.github.com>
2025-04-17 11:38:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET KEY")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
